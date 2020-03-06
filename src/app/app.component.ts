9781754edf9c68e1144279ee412e0bbc8568b9cd
JonathanLopez <toribioj818@gmail.com>
2020-03-06 19:08:34 -0400

    Este es el crud de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud';
+  title = 'Angular Crud';
+
+  employees = [
+    {'name': 'Fazt', position: 'manager'},
+    {'name': 'Tomas', position: 'Designer'},
+    {'name': 'Ramon', position: 'Programmer'},
+    {'name': 'Jose', position: 'Full Stack'}
+  ];
+
+  model:any = {};
+
+  addEmployee(): void {
+
+  }
+
+  deleteEmployee(): void {
+
+  }
+
+  editEmployee(): void {
+
+  }
+
+  updateEmployee(): void {
+
+  }
 }
